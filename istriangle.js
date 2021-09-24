36942ae80566a8ad87234a8cb3dcf0a349209e8b
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-24 11:40:34 +0530

    Update: ex-3

--- istriangle.js
+++ istriangle.js
@@ -2,9 +2,18 @@ const inputAngles = document.querySelectorAll(".input-angle");
 const isTriangle = document.querySelector("#is-triangle");
 const outputMessage = document.querySelector("#output-message");
 
-console.log(inputAngles);
-
 function checkTrianglepossible(){
+
+    const sumOfAngles = Number(inputAngles[0].value)+Number(inputAngles[1].value)+Number(inputAngles[2].value);
+    console.log(sumOfAngles);
+       
+    if(sumOfAngles === 180)
+    {
+        console.log("Hey! It will form the triangle");
+    }
+    else {
+        console.log("Hey! with this angles triangle not possible");
+    }
 
 }
 isTriangle.addEventListener("click", checkTrianglepossible )
