c82e67b4575fb0f0c2a1665b55f5062a537149c9
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-24 11:51:32 +0530

    Error Message and DOM output

--- istriangle.js
+++ istriangle.js
@@ -2,19 +2,19 @@ const inputAngles = document.querySelectorAll(".input-angle");
 const isTriangle = document.querySelector("#is-triangle");
 const outputMessage = document.querySelector("#output-message");
 
-function checkTrianglepossible(){
-
-    const sumOfAngles = Number(inputAngles[0].value)+Number(inputAngles[1].value)+Number(inputAngles[2].value);
-    console.log(sumOfAngles);
-       
-    if(sumOfAngles === 180)
+function checkTrianglepossible() {
+    if (inputAngles[0].value && inputAngles[1].value && inputAngles[2].value) 
     {
-        console.log("Hey! It will form the triangle");
-    }
-    else {
-        console.log("Hey! with this angles triangle not possible");
-    }
+        const sumOfAngles = Number(inputAngles[0].value) + Number(inputAngles[1].value) + Number(inputAngles[2].value);
 
+        if (sumOfAngles === 180) {
+            outputMessage.innerText = "Hey! with these angles can form the triangle";
+        } else {
+            outputMessage.innerText = "Hoo! with these angles can't form the triangle";
+        }
+    } else {
+        outputMessage.innerText = "All three angles are required! otherwise we can't processed";
+    }
+    
 }
-isTriangle.addEventListener("click", checkTrianglepossible )
-
+isTriangle.addEventListener("click", checkTrianglepossible)
