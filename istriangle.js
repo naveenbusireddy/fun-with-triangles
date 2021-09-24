06c5b04345c9a023f2e6951bdc17c71e3f87fa64
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-24 10:47:43 +0530

    initial commit

---
+++ istriangle.js
@@ -0,0 +1,11 @@
+const inputAngles = document.querySelectorAll(".input-angle");
+const isTriangle = document.querySelector("#is-triangle");
+const outputMessage = document.querySelector("#output-message");
+
+console.log(inputAngles);
+
+function checkTrianglepossible(){
+
+}
+isTriangle.addEventListener("click", checkTrianglepossible )
+
