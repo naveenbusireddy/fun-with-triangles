569cb70bfc5bc6ae7b96eb1a654702229148b9a4
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-24 16:25:20 +0530

    html form

--- trianglequiz.js
+++ trianglequiz.js
@@ -1,1 +1,8 @@
-const 
+const quizForm = document.querySelector(".quiz-form");
+const checkScore = document.querySelector("#check-scor");
+const showMessage = document.querySelector("#show-message");
+
+function checkQuizScore(){
+
+}
+checkScore.addEventListener("click", checkQuizScore)
