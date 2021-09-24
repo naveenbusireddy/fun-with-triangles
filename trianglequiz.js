01fd8c7154cb968eb2f87f20273ac0088feace48
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-24 16:56:22 +0530

    Feature: Quiz form
    Quiz form update

--- trianglequiz.js
+++ trianglequiz.js
@@ -1,8 +1,20 @@
 const quizForm = document.querySelector(".quiz-form");
-const checkScore = document.querySelector("#check-scor");
+const checkScore = document.querySelector("#check-score");
 const showMessage = document.querySelector("#show-message");
 
-function checkQuizScore(){
+const correctAnswers = ["90°", "right angled"];
 
+function checkQuizScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for (let value of formResults.values()) {
+        if (value === correctAnswers[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    showMessage.innerText = "Hey your score is: "+score;
 }
-checkScore.addEventListener("click", checkQuizScore)
+
+checkScore.addEventListener("click", checkQuizScore);
