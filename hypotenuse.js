4e4d86a26f12b92e059c154a012ac4883958d0f0
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-25 11:51:25 +0530

    initial commit

--- hypotenuse.js
+++ hypotenuse.js
@@ -2,8 +2,11 @@ const lengths = document.querySelectorAll(".input-lengths");
 const checkHypotenuse = document.querySelector("#check-hypotenuse");
 const showMessage = document.querySelector("#output");
 
+
 function calculateHypotenuse() {
-    
+    const hypotenuse = Math.hypot(Number(lengths[0].value), Number(lengths[1].value));
+
+    showMessage.innerText = "The length of hypotenuse is: " + hypotenuse;
 }
 
 checkHypotenuse.addEventListener("click", calculateHypotenuse);
