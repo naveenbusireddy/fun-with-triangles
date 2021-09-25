4ecf4cf6bd8cef8a66e75963b7784854e1f9761d
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-25 12:49:33 +0530

    initial commit

---
+++ areaoftriangle.js
@@ -0,0 +1,10 @@
+const base = document.querySelector("#base-value");
+const height = document.querySelector("#heght-value");
+const checkArea = document.querySelector("#check-area");
+const showMessage = document.querySelector("#output");
+
+function calculateArea() {
+    
+}
+
+checkArea.addEventListener("click", calculateArea);
