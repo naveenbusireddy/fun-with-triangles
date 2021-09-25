0502b4f9edac04a7a7006e140fc456c49b8e292e
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-25 13:10:35 +0530

    update: area of triangle

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -1,10 +1,13 @@
 const base = document.querySelector("#base-value");
-const height = document.querySelector("#heght-value");
+const height = document.querySelector("#height-value");
 const checkArea = document.querySelector("#check-area");
 const showMessage = document.querySelector("#output");
 
 function calculateArea() {
-    
+
+    const triangleArea = 0.5*(Number(base.value)*Number(height.value));
+    console.log(triangleArea);
+
 }
 
 checkArea.addEventListener("click", calculateArea);
