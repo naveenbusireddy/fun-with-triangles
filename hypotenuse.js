269a6fab8bfefbd7d5207e5d2e54ec295861813a
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-25 11:57:17 +0530

    update: variable change

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,9 +4,9 @@ const showMessage = document.querySelector("#output");
 
 
 function calculateHypotenuse() {
-    const hypotenuse = Math.hypot(Number(lengths[0].value), Number(lengths[1].value));
+    const lengthOfHypotenuse = Math.hypot(Number(lengths[0].value), Number(lengths[1].value));
 
-    showMessage.innerText = "The length of hypotenuse is: " + hypotenuse;
+    showMessage.innerText = "The length of hypotenuse is: " + lengthOfHypotenuse;
 }
 
 checkHypotenuse.addEventListener("click", calculateHypotenuse);
