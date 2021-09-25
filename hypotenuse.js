982aef894a1266a63e5cf87d264588c4fb67ebbc
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-25 11:15:41 +0530

    initial commit

---
+++ hypotenuse.js
@@ -0,0 +1,9 @@
+const lengths = document.querySelectorAll(".input-lengths");
+const checkHypotenuse = document.querySelector("#check-hypotenuse");
+const showMessage = document.querySelector("#output");
+
+function calculateHypotenuse() {
+    
+}
+
+checkHypotenuse.addEventListener("click", calculateHypotenuse);
