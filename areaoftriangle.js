ca0bed18ecdcb65ac0339ed684eb164fffe39e7b
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-25 13:15:53 +0530

    update: Error message

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -4,9 +4,12 @@ const checkArea = document.querySelector("#check-area");
 const showMessage = document.querySelector("#output");
 
 function calculateArea() {
-
-    const triangleArea = 0.5*(Number(base.value)*Number(height.value));
-    console.log(triangleArea);
+    if (base.value && height.value) {
+        const triangleArea = 0.5 * (Number(base.value) * Number(height.value));
+        showMessage.innerText = "Area of Triangle is: " + triangleArea;
+    } else {
+        showMessage.innerText = "Base and Height of the triangle is required to calculate the area of triangle";
+    }
 
 }
 
