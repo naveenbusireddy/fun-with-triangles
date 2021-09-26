3da2136798a902c181450715af4cc6c4ccc43d1b
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-26 18:00:02 +0530

    Quiz update
    Quiz questions update

--- trianglequiz.js
+++ trianglequiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const checkScore = document.querySelector("#check-score");
 const showMessage = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled"];
+const correctAnswers = ["90°", "right angled", "acute", "equilateral", "perimeter", "hypotenuse", "5", "180", "60", "5"];
 
 function checkQuizScore() {
     let score = 0;
