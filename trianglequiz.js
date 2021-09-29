ac846f9ce162a381c776e07bb2eaafe5c5acb599
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-29 09:45:49 +0530

    Update

--- trianglequiz.js
+++ trianglequiz.js
@@ -5,16 +5,19 @@ const showMessage = document.querySelector("#output");
 const correctAnswers = ["90°", "right angled", "acute", "equilateral", "perimeter", "hypotenuse", "5", "180°", "60°", "6"];
 
 function checkQuizScore() {
+        
     let score = 0;
     let index = 0;
     const formResults = new FormData(quizForm);
-    for (let value of formResults.values()) {
-        if (value === correctAnswers[index]) {
-            score = score + 1;
+
+     for (let value of formResults.values()) {
+            if (value === correctAnswers[index]) {
+                score = score + 1;
+            }
+            index = index + 1;
         }
-        index = index + 1;
-    }
-    showMessage.innerText = "Hey your score is: " + score;
+        showMessage.innerText = "Hey your score is: " + score;
+    
 }
 
 checkScore.addEventListener("click", checkQuizScore);
